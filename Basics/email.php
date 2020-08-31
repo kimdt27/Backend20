a30a65e8b2d51a5d2e8696e7a3d513ccf5b1f2ed
KT <kt@easv.dk>
2020-08-31 09:49:51 +0200

    Email validation

--- Basics/email.php
+++ Basics/email.php
@@ -5,6 +5,9 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $msg = $_POST['message'];
 
+$RegXp = "";
+
+
 if(isset($_POST['submit'])){
     $body = "$msg \n\n Name: $name\n Email: $email";
     mail($myMail,$subject,$body,"From: $email\n");
